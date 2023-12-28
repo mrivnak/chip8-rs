354ccea419207e3bf8ca9a747c9e04d9a5105b95
Mike Rivnak <rivnakm1@gmail.com>
2023-12-28 15:09:12 -0500

    fix tests

--- src/gpu.rs
+++ src/gpu.rs
@@ -5,7 +5,7 @@ pub const DISPLAY_SIZE: usize = DISPLAY_HEIGHT * DISPLAY_WIDTH;
 const PIXEL_COLOR_ON: (u8, u8, u8, u8) = (0xFF, 0xFF, 0xFF, 0xFF);
 const PIXEL_COLOR_OFF: (u8, u8, u8, u8) = (0x00, 0x00, 0x00, 0xFF);
 
-#[derive(Copy, Clone)]
+#[derive(Copy, Clone, Debug, PartialEq)]
 pub enum Pixel {
     On,
     Off,
